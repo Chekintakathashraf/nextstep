6f88cbe8bded3c020d12f5eca5fe78ed7a5cdf9c
maasha <ashrafchekintakath@gmail.com>
2022-08-17 17:56:24 +0530

    added something

--- nextstep/settings.py
+++ nextstep/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fmprnlozw%l=-=%8knfy6$z=9o$#ozg(7o%9m(35v)y63eaxxw'
+# SECRET_KEY = 'django-insecure-fmprnlozw%l=-=%8knfy6$z=9o$#ozg(7o%9m(35v)y63eaxxw'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = config('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -158,13 +162,20 @@ MESSAGE_TAGS = {
 
 #SMTP configuration
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ashrafchekintakath@gmail.com'
-EMAIL_HOST_PASSWORD = 'fvqnsjafpvjcbjiy'
-EMAIL_USE_TLS = True
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'ashrafchekintakath@gmail.com'
+# EMAIL_HOST_PASSWORD = 'fvqnsjafpvjcbjiy'
+# EMAIL_USE_TLS = True
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
 
-
-RAZOR_KEY_ID = 'rzp_test_51zNiPi6NnxprP'
-KEY_SECRET = 'xxEAry2EF7yXD852VyExL5rV'
+# RAZOR_KEY_ID = 'rzp_test_51zNiPi6NnxprP'
+# KEY_SECRET = 'xxEAry2EF7yXD852VyExL5rV'
+RAZOR_KEY_ID = config('RAZOR_KEY_ID')
+KEY_SECRET = config('KEY_SECRET')
